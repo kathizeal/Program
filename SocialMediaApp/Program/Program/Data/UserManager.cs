81f14887abc61fce06452464683cb78dc31f49de
kathizeal <65399572+kathizeal@users.noreply.github.com>
2021-03-29 14:57:04 +0530

    Add files via upload

---
+++ SocialMediaApp/Program/Program/Data/UserManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Program.Model;
+
+namespace Program.Data
+{
+    public sealed class UserManager
+    {
+        private User _CurrentUser;
+
+        private static UserManager _Instance = null;
+
+        private List<User> _UserLists = new List<User>();
+       
+        private UserManager() { }
+
+        public static UserManager GetInstance()
+        {
+            if (_Instance == null)
+            {
+                _Instance = new UserManager();
+
+            }
+            return _Instance;
+        }
+
+        public User CurrentUser
+        {
+            get
+            {
+                return _CurrentUser;
+            }
+            private set
+            {
+                _CurrentUser = value;
+            }
+        }
+        public void AddUser(String username, String password)
+        {
+
+            _UserLists.Add(new User(username, password));
+
+        }
+
+
+        public bool LoginUser(string username, string password)
+        {
+
+            foreach (var user in _UserLists)
+            {
+                if (user.UserName ==username  && user.Password == password)
+                {
+                    CurrentUser = user;
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+       
+        public void Logout()
+        {
+            _CurrentUser = null;
+        }
+        public string FindUser(long id)
+        {
+            
+            foreach(var user in _UserLists)
+            {
+                if (user.UserId == id)
+                    return user.UserName;
+                
+            }
+            return "No User found";
+        }
+
+    }
+}
---
+++ SocialMediaApp/Program/Program/Model/Comment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Model
+{
+    public class Comment
+    {
+
+        public Comment(long postid, string content, string username, long userid,long? parentid)
+        {
+
+            PostId = postid;
+            CommentContent = content;
+            CommenterName = username;
+            CommenterId = userid;
+            CommentId = DateTime.Now.Ticks;
+            CreatedTime = DateTime.UtcNow;
+            ParentCommentId = parentid;
+
+
+        }
+        
+        public long? ParentCommentId { get; set; }
+        public string CommenterName { get; set; }
+        public long CommenterId { get; set; }
+
+        public long CommentId { get; set; }
+        public string Title { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public long PostId { get; set; }
+
+        //private List<Comment> _Comments =new List<Comment>();
+        //public List<Comment> Comments { get { return _Comments; } set { _Comments = value; } }
+
+        
+    }
+    
+
+}
